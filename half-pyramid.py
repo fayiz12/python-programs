a9827dd1e45d965b4edc71f93024af591dba2742
Fayiz <77289134+fayiz12@users.noreply.github.com>
2023-09-28 19:48:02 +0530

    Create half-pyramid.py

---
+++ half-pyramid.py
@@ -0,0 +1,16 @@
+
+*
+**
+***
+****
+*****
+
+
+
+# Define the number of rows for the half pyramid
+num_rows = 5  # You can change this value to adjust the height of the pyramid
+
+# Loop through each row
+for i in range(1, num_rows + 1):
+    # Print '*' characters in each row
+    print('*' * i)
