79d2dd97a48063fe63ad5f8100b73f02d0237ab7
Fayiz <77289134+fayiz12@users.noreply.github.com>
2023-10-03 20:37:38 +0530

    Create numberPyramid.py

---
+++ numberPyramid.py
@@ -0,0 +1,34 @@
+
+def print_number_pyramid(n):
+    for i in range(1, n + 1):
+        # Print spaces before the numbers
+        for j in range(n - i):
+            print(" ", end=" ")
+        
+        # Print numbers in ascending order
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        
+        # Print numbers in descending order
+        for j in range(i - 1, 0, -1):
+            print(j, end=" ")
+        
+        # Move to the next line for the next row
+        print()
+
+try:
+    n = int(input("Enter the number of rows for the pyramid: "))
+    print_number_pyramid(n)
+except ValueError:
+    print("Please enter a valid number.")
+
+
+"""
+output:-
+        1 
+      1 2 1 
+    1 2 3 2 1 
+  1 2 3 4 3 2 1 
+1 2 3 4 5 4 3 2 1 
+
+"""
