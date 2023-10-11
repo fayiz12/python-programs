28b6bb6a5605e4375eef4cbacd24f87057d34518
Fayiz <77289134+fayiz12@users.noreply.github.com>
2023-10-11 20:01:40 +0530

    Create NumberDiamond.py

---
+++ NumberDiamond.py
@@ -0,0 +1,25 @@
+def print_number_diamond(n):
+    if n % 2 == 0:
+        n += 1  # Make sure n is odd
+
+    for i in range(1, n + 1, 2):
+        spaces = (n - i) // 2
+        numbers = list(range(1, i + 1))
+
+        # Print leading spaces
+        print(" " * spaces, end="")
+
+        # Print numbers in increasing order
+        for num in numbers:
+            print(num, end="")
+
+        # Print numbers in decreasing order, excluding the last number
+        for num in reversed(numbers[:-1]):
+            print(num, end="")
+
+        # Print trailing spaces and move to the next line
+        print(" " * spaces)
+
+if __name__ == "__main__":
+    n = int(input("Enter the number of rows for the diamond pattern: "))
+    print_number_diamond(n)
